03524a377b4d1bfbcea8e010e1fb3effe9a363fc
CptJackFrost <cptjackfrost74@gmail.com>
2020-07-02 21:38:32 +0500

    Первый коммит. Создан объект

---
+++ src/main/java/somepackage/Other.java
@@ -0,0 +1,7 @@
+package main.java.somepackage;
+
+public class Other {
+    public static void saySomething(){
+        System.out.println("А теперь домашнее заданее");
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import main.java.somepackage.Other;
+import main.java.somepackage.Some;
+import main.java.somethinggreen.Contract;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+    }
+}
